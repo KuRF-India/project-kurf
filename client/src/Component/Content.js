3d48cba58c481626386423b5697e1005513bd492
KuRF-India <kurfgenhq@gmail.com>
2023-05-25 23:37:57 +0530

    Fixed: contain spacing

--- client/src/Component/Content.js
+++ client/src/Component/Content.js
@@ -10,16 +10,16 @@ const Content = () => {
         id="articles"
       >
         <div className="container mx-auto flex px-10 md:py-36 md:flex-row flex-col-reverse items-center ">
-          <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+          <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-6 md:mb-0 items-center text-center">
             <h1 className="title-font sm:text-4xl text-3xl mb-2 font-medium text-white">
               “SHINE THE NATION”
               <br className="hidden lg:inline-block" />
             </h1>
-            <p className="ml-2 mb-8 leading-relaxed lg:w-3/4">
+            <p className="ml-2 mb-2 md:mb-8 leading-relaxed lg:w-3/4">
               Kuki Research Forum is an intellectual group of Kukis` in India
             </p>
             <div className="flex justify-center">
-              <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg mt-8">
+              <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg md:mt-8">
                 Coming Soon
               </button>
             </div>
